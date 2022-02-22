4f9e307e8c68250f49d81fea0167f1b7f77dc1c0
anthonychang26 <100131273+anthonychang26@users.noreply.github.com>
2022-02-22 18:24:58 +0800

    Create Hw2.cpp

---
+++ Hw2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+       
+    cout <<  "hello Anthony ";
+    cout <<  "my height is " << 2 << "m " << 00 << "cm";
+
+    return 0;
+}
