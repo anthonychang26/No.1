845f794378fd23e3cd6225744708bb735e7c466b
anthonychang26 <100131273+anthonychang26@users.noreply.github.com>
2022-02-22 18:48:50 +0800

    Create Hw3.cpp

---
+++ Hw3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myAge = 35;
+  cout << "I am " << myAge << " years old.";
+  int myHeight=160;
+  cout << "My height is  " << myHeight << " cm.";
+  int myWeight=60;
+  cout << "My weight is  " << myWeight << " kg.";
+  int myBrother=1;
+  cout << "I have " << myBrother << " brother.";
+  int mySister=2;
+  cout << "I have  " << mySister << " sister.";
+}
