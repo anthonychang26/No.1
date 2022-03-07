151be7aacfb63f0812fd0505b815ffb3fc153263
anthonychang26 <100131273+anthonychang26@users.noreply.github.com>
2022-03-07 19:24:17 +0800

    Create ex4.cpp

---
+++ ex4.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (void) {
+     printf(" hello Ugly Jun "); 
+     printf(" my height is %d m %d cm \n",1,80);
+     printf(" this building is %20d cm \n", 890);
+     printf(" this building is %20d cm \n", 7890);
+     printf(" this building is %20d cm \n", 67890);
+     printf(" this building is %20d cm \n", 567890);
+     printf(" this building is %20d cm \n", 1234567890);
+     printf(" pi = %2.14f \n ", 3.1415926535897932384626433832795);
+}
